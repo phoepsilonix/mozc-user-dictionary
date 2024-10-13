7a7afc08e81fe31f20446c96388e25bf4c572480
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-10-14 01:38:31 +0900

    Added Generate User Dictionary

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# mozc-user-dictionary
+# SudachiDict and Neologd Mozc ユーザー辞書
+
+SudachiDict[^3][^4]をユーザー辞書形式に変換したものと、Neologdが提供しているmecab-ipadic-neologd[^1],mecab-unidic-neologd[^2]を、一つのユーザー辞書形式にまとめたものの、2つを公開しています。
+
+[^1]: https://github.com/neologd/mecab-ipadic-neologd
+[^2]: https://github.com/neologd/mecab-unidic-neologd
+[^3]: https://worksapplications.github.io/Sudachi/
+[^4]: https://github.com/WorksApplications/SudachiDict
--- userdic/mkdict.sh
+++ userdic/mkdict.sh
@@ -6,35 +6,62 @@ tar xf dict-to-mozc-0.3.0-x86_64-unknown-linux-gnu.tar.gz --strip-component=1
 curl -LO https://github.com/google/mozc/raw/refs/heads/master/src/data/dictionary_oss/id.def
 PATH=$HOME/.cargo/bin:$PATH
 
+USERDIC=user_dic-Neologd
 # unidic
-SYSTEMDIC=mecab-unidic-user-dict-seed.20200910.csv
-USERDIC=user_dic-unidic
-curl -LO https://github.com/phoepsilonix/mecab-unidic-neologd/raw/refs/heads/master/seed/$SYSTEMDIC.xz
+SYSTEMDIC1=mecab-unidic-user-dict-seed.20200910.csv
+USERDIC1=user_dic-unidic
+curl -LO https://github.com/phoepsilonix/mecab-unidic-neologd/raw/refs/heads/master/seed/$SYSTEMDIC1.xz
 curl -L -o LICENSE.neologd-unidic https://github.com/phoepsilonix/mecab-unidic-neologd/raw/refs/heads/master/COPYING
-xz -d -k $SYSTEMDIC.xz
-./dict-to-mozc -n -U -i ./id.def -f ./$SYSTEMDIC > ./$USERDIC
-ls -la $USERDIC*
-split --numeric-suffixes=1 -l 1000000 --additional-suffix=.txt $USERDIC $USERDIC-
-mkdir -p ../release
-[[ -e ../release/${USERDIC}.tar.xz ]] && rm ../release/${USERDIC}.tar.xz
-
-tar cf ../release/${USERDIC}.tar ${USERDIC}-*.txt LICENSE.neologd-unidic
-xz -9 -e ../release/${USERDIC}.tar
-
-rm $USERDIC $USERDIC-*.txt ./$SYSTEMDIC
+xz -d -k $SYSTEMDIC1.xz
+./dict-to-mozc -n -U -i ./id.def -f ./$SYSTEMDIC1 > ./$USERDIC1
+ls -la $USERDIC1*
 
 # ipadic
-SYSTEMDIC=mecab-user-dict-seed.20200910.csv
-USERDIC=user_dic-ipadic
-curl -LO https://github.com/phoepsilonix/mecab-ipadic-neologd/raw/refs/heads/master/seed/$SYSTEMDIC.xz
+SYSTEMDIC2=mecab-user-dict-seed.20200910.csv
+USERDIC2=user_dic-ipadic
+curl -LO https://github.com/phoepsilonix/mecab-ipadic-neologd/raw/refs/heads/master/seed/$SYSTEMDIC2.xz
 curl -L -o LICENSE.neologd-ipadic https://github.com/phoepsilonix/mecab-ipadic-neologd/raw/refs/heads/master/COPYING
-xz -d -k $SYSTEMDIC.xz
-./dict-to-mozc -n -P 12 -N 10 -U -i ./id.def -f ./$SYSTEMDIC > ./$USERDIC
+xz -d -k $SYSTEMDIC2.xz
+./dict-to-mozc -n -P 12 -N 10 -U -i ./id.def -f ./$SYSTEMDIC2 > ./$USERDIC2
+ls -la $USERDIC2*
+
+# sudachidict
+SYSTEMDIC3=sudachi.csv
+USERDIC3=user_dic-SudachiDict
+_sudachidict_date=$(curl -s 'http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict-raw/' | grep -o '<td>[0-9]*</td>' | grep -o '[0-9]*' | sort -n | tail -n 1)
+curl -L -o LICENSE.SudachiDict https://github.com/WorksApplications/SudachiDict/raw/refs/heads/develop/LICENSE-2.0.txt
+curl -L -o LEGAL.SudachiDict https://github.com/WorksApplications/SudachiDict/raw/refs/heads/develop/LEGAL
+curl -LO "http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict-raw/${_sudachidict_date}/small_lex.zip"
+curl -LO "http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict-raw/${_sudachidict_date}/core_lex.zip"
+curl -LO "http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict-raw/${_sudachidict_date}/notcore_lex.zip"
+unzip -x small_lex.zip
+unzip -x core_lex.zip
+unzip -x notcore_lex.zip
+cat small_lex.csv core_lex.csv notcore_lex.csv > sudachi.csv
+rm *_lex.csv
+./dict-to-mozc -s -U -i ./id.def -f ./$SYSTEMDIC3 > ./$USERDIC3
+
+cat $USERDIC1 $USERDIC2 > mozc-user_dict.txt
+awk 'BEGIN{
+    FS="\t"
+    OFS="\t"
+}
+{
+    if (!a[$1,$2,$3]++) {
+        print $0
+    }
+}' mozc-user_dict.txt > $USERDIC
 ls -la $USERDIC*
 split --numeric-suffixes=1 -l 1000000 --additional-suffix=.txt $USERDIC $USERDIC-
 mkdir -p ../release
 [[ -e ../release/${USERDIC}.tar.xz ]] && rm ../release/${USERDIC}.tar.xz
-tar cf ../release/${USERDIC}.tar ${USERDIC}-*.txt LICENSE.neologd-ipadic
+tar cf ../release/${USERDIC}.tar ${USERDIC}-*.txt LICENSE.neologd-unidic LICENSE.neologd-ipadic
 xz -9 -e ../release/${USERDIC}.tar
 
-rm $USERDIC $USERDIC-*.txt ./$SYSTEMDIC
+split --numeric-suffixes=1 -l 1000000 --additional-suffix=.txt $USERDIC3 $USERDIC3-
+mkdir -p ../release
+[[ -e ../release/${USERDIC3}.tar.xz ]] && rm ../release/${USERDIC3}.tar.xz
+tar cf ../release/${USERDIC3}.tar ${USERDIC3}-*.txt LICENSE.SudachiDict LEGAL.SudachiDict
+xz -9 -e ../release/${USERDIC3}.tar
+
+rm $USERDIC $USERDIC-*.txt $USERDIC1 $USERDIC2 $USERDIC3 $SYSTEMDIC1 $SYSTEMDIC2 $SYSTEMDIC3
